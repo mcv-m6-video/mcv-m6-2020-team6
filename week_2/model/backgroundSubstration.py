57973f50cb8640ede1e80b963b793146fe1e1795
Claudia Baca Pérez <claudia.bacaperez@e-campus.uab.edu>
2020-03-11 18:51:25 +0100

    change directory

--- week_2/model/backgroundSubstration.py
+++ week_2/model/backgroundSubstration.py
@@ -507,7 +507,7 @@ def background_adaptive_gaussian():
 
     frames_dir = '../datasets/train/S03/c010/frames/'
     roi_path = '../datasets/train/S03/c010/roi.jpg'
-    gt_dir = '../datasets/train/S03/c010/gt/gt.txt'
+    gt_dir = 'annotation_fix.txt'
 
     train_list, test_list = get_frames(frames_dir, trainbackground=0.25)
     image_list_fg = fg_mask_single_gaussian(frames_dir, roi_path, alpha=4.75, adaptive=True, rho=1)
